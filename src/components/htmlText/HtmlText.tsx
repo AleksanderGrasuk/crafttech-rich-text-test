1b47d59f2caa0d843e47345a40e95cbf2e149d3c
grasiuk <a.grasiuk@crafttech.ru>
2025-03-28 16:31:29 +0300

    fix rendedrImage and moving reactQuill in Htmltext, add styles for htmltext

--- src/components/htmlText/HtmlText.tsx
+++ src/components/htmlText/HtmlText.tsx
@@ -1,20 +1,103 @@
-import { forwardRef } from "react";
-import { IHtmlTextProps } from "../../types/types";
+import React, {
+  forwardRef,
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.bubble.css";
+import "./HtmlText.scss";
+
+interface IHtmlTextProps {
+  html: string;
+  id: string;
+  width: number;
+  height: number;
+  isEditing: boolean;
+  onChange: (value: string) => void;
+  onExitEditing: () => void;
+}
 
 const HtmlText = forwardRef<HTMLDivElement, IHtmlTextProps>(
-  ({ html, id }, ref) => {
+  ({ html, id, width, height, isEditing, onChange, onExitEditing }, ref) => {
+    const quillRef = useRef<ReactQuill>(null);
+    const [value, setValue] = useState(html);
+
+    const handleKeyDown = useCallback(
+      (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          onExitEditing();
+        }
+      },
+      [onExitEditing]
+    );
+
+    useEffect(() => {
+      if (isEditing) {
+        document.addEventListener("keydown", handleKeyDown);
+      } else {
+        document.removeEventListener("keydown", handleKeyDown);
+      }
+
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [isEditing, handleKeyDown]);
+
+    useEffect(() => {
+      setValue(html);
+    }, [html]);
+
     return (
       <div
         id={`htmltext_${id}`}
-        dangerouslySetInnerHTML={{ __html: html }}
-        style={{
-          position: "fixed",
-          overflow: "hidden",
-          left: "100000px",
-          top: "100000px",
-        }}
         ref={ref}
-      ></div>
+        className={`htmltext-container ${isEditing ? "editing" : ""}`}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+        }}
+      >
+        {isEditing ? (
+          <ReactQuill
+            ref={quillRef}
+            value={value}
+            onChange={(val) => {
+              setValue(val);
+              onChange(val);
+            }}
+            theme="bubble"
+            modules={{
+              toolbar: [
+                [{ header: 1 }, { header: 2 }, "bold", "italic", "underline"],
+                [{ list: "ordered" }, { list: "bullet" }, "clean"],
+                [
+                  { size: ["small", false, "large", "huge"] },
+                  { color: ["red", "green", "blue"] },
+                  { align: ["justify", "center", "right", "left"] },
+                ],
+              ],
+            }}
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+          />
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+              width: "100%",
+              textAlign: "center",
+            }}
+            dangerouslySetInnerHTML={{ __html: value }}
+          />
+        )}
+      </div>
     );
   }
 );
