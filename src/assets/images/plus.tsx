04ce808079cd645e0300b459185b89479317b09d
grasiuk <a.grasiuk@crafttech.ru>
2025-03-31 11:46:27 +0300

    fix svg

--- src/assets/images/plus.tsx
+++ src/assets/images/plus.tsx
@@ -5,17 +5,17 @@ const PlusIcon = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g
       id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     ></g>
     <g id="SVGRepo_iconCarrier">
       <path
         className="plus-icon_color"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M18 6H6V18H18V6ZM4.5 4.5V19.5H19.5V4.5H4.5Z"
         fill="none"
       ></path>
